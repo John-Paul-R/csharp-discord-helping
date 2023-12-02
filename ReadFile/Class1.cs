4fe03fc250439b3ac8b26478563d634b546e4f39
John Paul Rutigliano <johnpaul122jpr@gmail.com>
2023-12-01 22:36:02 -0500

    Create CharLineStreamReader, ReadFile project

---
+++ ReadFile/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System.Text;
+using ReadFile;
+
+string filePath = "file.txt";
+
+using FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+
+using var reader = new CharLineStreamReader(fs, Encoding.UTF8);
+
+char[]? charArray = reader.ReadLineToCharArray();
+
+// wew, your span, read directly from the stream
+Span<char> chars = charArray;
+
+Console.WriteLine(new string(charArray));
